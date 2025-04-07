1fc120c7b1ff80715ffededdb2880f887a10e618
Емельянов Максим <masiko-17@mail.ru>
2025-04-07 22:19:35 +0500

    Создаст routing сайта

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,19 +1,39 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "../widgets/Header/Header"
-import { Button, Stack } from "@chakra-ui/react"
+import News from '@/page/News/News';
+import Main from '@/page/Main/Main';
+import Calendar from '@/page/Calendar/Calendar';
+import Development from '@/page/Development/Development';
+import Workers from '@/page/Workers/Workers';
+import Communication from '@/page/Communication/Communication';
+import Services from '@/page/Services/Services';
 
 function App() {
 
   return (
-    <>
-      <div>
-        <Header />
-        <Stack>
-          <Button>1</Button>
-          <Button>2</Button>
-          <Button>3</Button>
-        </Stack>
-      </div>
-    </>
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/news' element={<News />} />
+
+        <Route path='/personal_calendar' element={<Calendar />} />
+        <Route path='/company_calendar' element={<Calendar />} />
+        <Route path='/meeting_calendar' element={<Calendar />} />
+
+        <Route path='/information_development' element={<Development />} />
+        <Route path='/training_development' element={<Development />} />
+        <Route path='/documents_development' element={<Development />} />
+
+        <Route path='/workers' element={<Workers />} />
+
+        <Route path='/forum_communication' element={<Communication />} />
+        <Route path='/communication_communication' element={<Communication />} />
+
+        <Route path='/services' element={<Services />} />
+      </Routes>
+    </Router>
   )
 }
 
