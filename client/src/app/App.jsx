86150234f3ffb93516198c4c7a7ddb7f55f6ce0d
EduardKostylev <edik03032005@yandex.ru>
2025-04-07 22:18:11 +0500

    Сделал Header

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -7,11 +7,6 @@ function App() {
     <>
       <div>
         <Header />
-        <Stack>
-          <Button>1</Button>
-          <Button>2</Button>
-          <Button>3</Button>
-        </Stack>
       </div>
     </>
   )
--- client/src/widgets/Header/Header.jsx
+++ client/src/widgets/Header/Header.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
 import './Header.scss'
-import { Stack } from '@chakra-ui/react'
+import { Input,InputGroup,IconButton, Avatar} from "@chakra-ui/react"
+import { Link } from 'react-router-dom';
+import { LuSearch } from "react-icons/lu"
+import { FaVk } from "react-icons/fa";
 
 export default function Header() {
     return (
-        <div className='header'>
-            <div className='header__nav-items'>nav</div>
+    <div class="header">
+        <div class="header__main">
+            <div class="header__main-left">
+            <a class="header__main-left-link">
+                <img
+                class="header__main-left-link-logo"
+                src="logo.png"
+                alt="logo"
+                />
+            </a>
+            <InputGroup className="header__search" flex="1" endElement={<LuSearch />}>
+                <Input placeholder="Поиск" />
+            </InputGroup>
+            </div>
+            <div class="header__main-right">
+            <div class="header__main-right-links">
+                <IconButton aria-label="VK" variant="gost" color="green.600">
+                <FaVk />
+                </IconButton>
+
+                <IconButton aria-label="VK" variant="gost" color="green.600">
+                <FaVk />
+                </IconButton>
+
+                <IconButton aria-label="VK" variant="gost" color="green.600">
+                <FaVk />
+                </IconButton>
+            </div>
+            <Avatar.Root>
+                <Avatar.Fallback name="Segun Adebayo" />
+                <Avatar.Image src="" />
+            </Avatar.Root>
+            </div>
         </div>
+        <div class="header__nav">nav</div>
+    </div>
     )
 }
