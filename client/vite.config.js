6372b4b20a765fc2e85a3fdeca58e65202ec392d
Емельянов Максим <masiko-17@mail.ru>
2025-04-30 14:12:32 +0500

    Изменит сборку проекта

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { resolve } from 'path'
+import { copy } from 'fs-extra'
+import { fileURLToPath } from 'url'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(fileURLToPath(new URL('.', import.meta.url)), 'index.html')
+      }
+    }
+  },
+
+  plugins: [react(),
+  {
+    name: 'copy-public-folder',
+    closeBundle: async () => {
+      const rootDir = fileURLToPath(new URL('.', import.meta.url))
+      await copy(
+        resolve(rootDir, 'public'),
+        resolve(rootDir, 'dist/public')
+      )
+    }
+  }
+  ],
 })
